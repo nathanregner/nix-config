446e49520b43e9ea2957af48a05d0497885622f2
Nathan Regner <nathanregner@gmail.com>
2024-01-27 15:22:38 -0700

    WIP tweak(voron): patch moonraker

--- overlays/default.nix
+++ overlays/default.nix
@@ -53,6 +53,35 @@ let
           ./moonraker/0001-file_manager-Add-config-option-to-rename-duplicate-f.patch
         ];
       });
+      # TODO: upstream https://github.com/Arksine/moonraker/issues/401
+      moonraker = prev.moonraker.overrideAttrs (oldAttrs: {
+        patches = oldAttrs.patches or [ ] ++ [
+          ./moonraker/0001-file_manager-Add-config-option-to-rename-duplicate-f.patch
+        ];
+      });
+      moonraker =
+        (prev.moonraker.override (prev: rec {
+          python3 = prev.python3.override {
+            packageOverrides =
+              self: super:
+              let
+                preprocess-cancellation =
+                  inputs.preprocess-cancellation.packages.${final.stdenv.hostPlatform.system}.default;
+              in
+              assert prev.python3.pkgs.hasPythonModule preprocess-cancellation;
+              {
+                inherit preprocess-cancellation;
+              };
+            self = python3;
+          };
+        })).overrideAttrs
+          (oldAttrs: {
+            patches = oldAttrs.patches or [ ] ++ [
+              # TODO: upstream https://github.com/Arksine/moonraker/issues/401
+              ./moonraker/0001-file_manager-Add-config-option-to-rename-duplicate-f.patch
+              ./moonraker-preprocess-cancellation.patch
+            ];
+          });
 
       nix-update-script = (
         args:
