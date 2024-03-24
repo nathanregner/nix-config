5a2ea372e9c840693f41b919760a6ddd18cbb71d
Nathan Regner <nathanregner@gmail.com>
2024-03-24 11:36:11 -0600

    generate client

--- pkgs/hydra-hooks/default.nix
+++ pkgs/hydra-hooks/default.nix
@@ -1,17 +1,29 @@
 # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md
-{ lib, rustPlatform }:
-rustPlatform.buildRustPackage {
-  pname = "hydra-hooks";
-  version = "1.0.0";
+{ lib, rustPlatform, buildPackages, }:
+let
+  pkg = rustPlatform.buildRustPackage {
+    pname = "hydra-hooks";
+    version = "1.0.0";
 
-  # src = lib.sources.sourceFilesBySuffices (lib.cleanSource ./.) [ ".nix" ];
-  src = lib.cleanSource ./.;
+    # src = lib.sources.sourceFilesBySuffices (lib.cleanSource ./.) [ ".nix" ];
+    src = lib.cleanSource ./.;
 
-  postPatch = ''
-    ln -sf ${./Cargo.toml} Cargo.toml
-    ln -sf ${./Cargo.lock} Cargo.lock
-  '';
+    postPatch = ''
+      ln -sf ${./Cargo.toml} Cargo.toml
+      ln -sf ${./Cargo.lock} Cargo.lock
+    '';
 
-  cargoLock.lockFile = ./Cargo.lock;
-}
+    cargoLock.lockFile = ./Cargo.lock;
+
+    passthru.devenv = pkg.overrideAttrs (old: {
+      nativeBuildInputs = old.nativeBuildInputs or [ ]
+        ++ (with buildPackages; [ openapi-generator-cli ]);
+      env = old.env or { } // {
+        HYDRA_API = "${buildPackages.hydra_unstable.src}/hydra-api.yaml";
+        GENERATE_HYDRA =
+          "openapi-generator-cli generate -i $HYDRA_API -g rust -o hydra-client";
+      };
+    });
+  };
+in pkg
 
