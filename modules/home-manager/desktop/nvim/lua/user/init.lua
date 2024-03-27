635afe9299f3b411b7385c1b0c329a3d32bd1914
Nathan Regner <nathan.regner@clickbank.com>
2024-03-27 12:31:48 -0600

    disable autopairs in cmdline

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -83,7 +83,9 @@ require("lazy").setup({
     "altermo/ultimate-autopair.nvim",
     event = { "InsertEnter", "CmdlineEnter" },
     branch = "v0.6", --recommended as each new version will have breaking changes
-    opts = {},
+    opts = {
+      cmap = false,
+    },
   },
 
   {
