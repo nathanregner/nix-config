846f9d1ea59b71cb09d52bcbe2997bd044e90146
Nathan Regner <nathan.regner@clickbank.com>
2024-08-08 15:15:04 -0600

    fixup! feat(nvim): add harper-ls

--- pkgs/harper-ls/default.nix
+++ pkgs/harper-ls/default.nix
@@ -1,26 +1,15 @@
 # TODO: upstream
-{
-  lib,
-  rustPlatform,
-  fetchFromGitHub,
-  cmake,
-  mkShell,
-}:
-let
-  pkg = rustPlatform.buildRustPackage rec {
-    pname = "harper-ls";
-    version = "0.9.1";
+{ rustPlatform, fetchFromGitHub }:
+rustPlatform.buildRustPackage rec {
+  pname = "harper-ls";
+  version = "0.9.2";
 
-    src = fetchFromGitHub {
-      owner = "elijah-potter";
-      repo = "harper";
-      rev = "v${version}";
-      sha256 = "sha256-Ojx5SpwoUbTdC4Aj4mNf8/ltpTDfL5e5rS260lJ3FZw=";
-    };
-
-    # nativeBuildInputs = [ cmake ];
-
-    cargoLock.lockFile = "${src}/Cargo.lock";
+  src = fetchFromGitHub {
+    owner = "elijah-potter";
+    repo = "harper";
+    rev = "v${version}";
+    sha256 = "sha256-XfyEp3PLLWq7yDV8UcMfJRyP39scuw94jwhGxuMz958=";
   };
-in
-pkg
+
+  cargoLock.lockFile = "${src}/Cargo.lock";
+}
