3e9dc1b657a7decdfef869d3fe1cad70fde11281
Nathan Regner <nathanregner@gmail.com>
2024-11-29 20:33:50 -0700

    wip(nvim): neoconf

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -189,10 +189,13 @@ require("lazy").setup({
 
   { "towolf/vim-helm", ft = "helm" },
 
+  { "folke/neoconf.nvim", opts = {} },
+
   { -- LSP Configuration & Plugins
     "neovim/nvim-lspconfig",
     dependencies = {
       "artemave/workspace-diagnostics.nvim",
+      "folke/neoconf.nvim",
       "j-hui/fidget.nvim",
       "yioneko/nvim-vtsls",
     },
@@ -426,6 +429,9 @@ require("lazy").setup({
 
   { -- Autoformat
     "stevearc/conform.nvim",
+    dependencies = {
+      "folke/neoconf.nvim",
+    },
     opts = {
       formatters_by_ft = {
         bash = { "shfmt" },
@@ -478,6 +484,27 @@ require("lazy").setup({
     },
     config = function(_, opts)
       require("conform").setup(opts)
+
+      local defaults = {
+        enabled = true,
+      }
+
+      -- https://github.com/folke/neoconf.nvim/blob/fbe717664a732ab9e62737216bd3d0b6d9f84dbf/lua/neoconf/plugins/lspconfig.lua
+      require("neoconf.plugins").register({
+        name = "conform",
+        on_schema = function(schema)
+          -- this call will create a json schema based on the lua types of your default settings
+          schema:import("myplugin", defaults)
+          -- Optionally update some of the json schema
+          schema:set("myplugin.array", {
+            description = "Special array containing booleans or numbers",
+            anyOf = {
+              { type = "boolean" },
+              { type = "integer" },
+            },
+          })
+        end,
+      })
 
       vim.api.nvim_create_user_command("FormatDisable", function(args)
         if args.bang then
