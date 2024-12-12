1c36b10b3f0b2fc6a86098f73716e2a00891ae6b
Nathan Regner <nathan.regner@clickbank.com>
2024-12-12 11:07:24 -0700

    re-enable autoFetch

--- modules/home-manager/desktop/cli/lazygit.nix
+++ modules/home-manager/desktop/cli/lazygit.nix
@@ -31,10 +31,6 @@
     ) { };
     # https://github.com/jesseduffield/lazygit/blob/master/docs/Config.md
     settings = {
-      git = {
-        autoFetch = false;
-      };
-
       gui = {
         nerdFontsVersion = "3";
       };
