a5be374bd1dbd601fe70802d6695c6f184e5948d
Nathan Regner <nathanregner@gmail.com>
2024-12-14 11:54:07 -0700

    wip: feat(nixos): enable ollama

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -746,25 +746,6 @@ require("lazy").setup({
   },
 
   {
-    "olimorris/codecompanion.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    opts = {
-      --Refer to: https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua
-      strategies = {
-        --NOTE: Change the adapter as required
-        chat = { adapter = "ollama" },
-        inline = { adapter = "ollama" },
-      },
-      opts = {
-        log_level = "DEBUG",
-      },
-    },
-  },
-
-  {
     "folke/which-key.nvim",
     config = function()
       require("which-key").setup({})
