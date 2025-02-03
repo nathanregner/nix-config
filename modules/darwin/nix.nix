0e83ddc9e2658ffd3484f19e0304917a2ff16e50
Nathan Regner <nathan.regner@clickbank.com>
2025-02-03 10:18:56 -0700

    fix(darwin): add /nix/store to extra-sandbox-paths

--- modules/darwin/nix.nix
+++ modules/darwin/nix.nix
@@ -12,7 +12,10 @@
   };
 
   # https://github.com/NixOS/nix/issues/4119#issuecomment-1734738812
-  nix.settings.sandbox = "relaxed";
+  nix.settings = {
+    sandbox = "relaxed";
+    extra-sandbox-paths = [ "/nix/store" ];
+  };
   system.systemBuilderArgs = lib.mkIf (config.nix.settings.sandbox == "relaxed") {
     sandboxProfile = ''
       (allow file-read* file-write* process-exec mach-lookup (subpath "${builtins.storeDir}"))
