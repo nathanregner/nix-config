6f6397d8b3548e229291f26ab58c7ac1e16d5d2a
Nathan Regner <nathan.regner@clickbank.com>
2025-02-13 16:47:23 -0700

    fix(nvim): disable noisy diagnostics

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -162,6 +162,7 @@ require("lazy").setup({
         return newVirtText
       end
 
+      ---@diagnostic disable-next-line: missing-fields
       require("ufo").setup({
         open_fold_hl_timeout = 150,
         close_fold_kinds_for_ft = {
@@ -557,6 +558,7 @@ require("lazy").setup({
   {
     "folke/which-key.nvim",
     config = function()
+      ---@diagnostic disable-next-line: missing-fields
       require("which-key").setup({})
       require("which-key").register({
         ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
@@ -957,6 +959,7 @@ require("lazy").setup({
       { "JoosepAlviste/nvim-ts-context-commentstring", opts = { enable_autocmd = false } },
     },
     config = function()
+      ---@diagnostic disable-next-line: missing-fields
       require("Comment").setup({
         pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
       })
@@ -1056,6 +1059,7 @@ require("lazy").setup({
     },
     config = function()
       local neotest = require("neotest")
+      ---@diagnostic disable-next-line: missing-fields
       neotest.setup({
         adapters = {
           require("neotest-rust")({
@@ -1068,6 +1072,7 @@ require("lazy").setup({
           enabled = true,
           severity = vim.diagnostic.severity.WARN,
         },
+        ---@diagnostic disable-next-line: missing-fields
         output = {
           open_on_run = true,
           enter = true,
