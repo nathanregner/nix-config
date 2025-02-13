e16b63f60b81a0a800c48ef58f7ca564d8dd2fb2
Nathan Regner <nathanregner@gmail.com>
2025-02-13 16:47:09 -0700

    fix(nvim): manage common treesitter grammars with nix

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -670,12 +670,13 @@ require("lazy").setup({
   {
     -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
+    dir = vim.g.nix.nvim_treesitter.dir,
+    pin = true,
     dependencies = {
       "nvim-treesitter/nvim-treesitter-context",
       "nvim-treesitter/nvim-treesitter-textobjects",
       "nushell/tree-sitter-nu",
     },
-    build = ":TSUpdate",
     keys = {
       {
         "[C",
@@ -685,6 +686,7 @@ require("lazy").setup({
     },
     lazy = false,
     opts = {
+      parser_install_dir = vim.g.nix.nvim_treesitter.parser_install_dir,
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
@@ -1290,6 +1292,9 @@ require("lazy").setup({
 }, {
   dev = {
     path = "~/dev/github",
+  },
+  performance = {
+    rtp = { paths = vim.g.nix.rtp },
   },
 })
 
---
+++ modules/home-manager/desktop/nvim/modules/treesitter.nix
@@ -0,0 +1,94 @@
+{
+  pkgs,
+  config,
+  ...
+}:
+let
+  parserPrefix = "nvim/nvim-treesitter";
+
+  package = pkgs.unstable.vimPlugins.nvim-treesitter.withPlugins (
+    plugins: with plugins; [
+      bash
+      c
+      clojure
+      cmake
+      cpp
+      css
+      devicetree
+      diff
+      git_rebase
+      gitcommit
+      gitignore
+      go
+      gomod
+      gosum
+      graphql
+      html
+      hyprlang
+      ini
+      java
+      javascript
+      json
+      kconfig
+      lua
+      make
+      markdown
+      meson
+      nginx
+      nix
+      pem
+      perl
+      python
+      query
+      rst
+      rust
+      sql
+      terraform
+      tmux
+      toml
+      tsx
+      typescript
+      udev
+      vim
+      vimdoc
+      vue
+      xml
+      yaml
+    ]
+  );
+in
+{
+  programs.neovim.lua.globals = (
+    let
+      parser_install_dir = "${config.xdg.dataHome}/${parserPrefix}";
+    in
+    {
+      nvim_treesitter = {
+        dir = "${package}";
+        inherit parser_install_dir;
+      };
+      rtp = [ parser_install_dir ];
+    }
+  );
+
+  programs.neovim.extraPackages = with pkgs.unstable; [
+    clang
+    gnumake
+  ];
+
+  xdg.dataFile = builtins.listToAttrs (
+    builtins.map (
+      grammar:
+      let
+        language = builtins.elemAt (builtins.match "vimplugin-treesitter-grammar-(.*)" grammar.name) 0;
+      in
+      {
+        name = "${parserPrefix}/parser/${language}.so";
+        value = {
+          source = "${grammar}/parser/${language}.so";
+          force = true;
+        };
+      }
+    ) package.passthru.dependencies
+  );
+}
