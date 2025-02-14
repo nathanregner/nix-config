c9b77237623b6831349ca0efeefa84560809bab9
Nathan Regner <nathanregner@gmail.com>
2025-02-13 18:05:40 -0700

    fixup! wip: feat(nvim): add snacks

--- modules/home-manager/desktop/nvim/lua/user/plugins/snacks.lua
+++ modules/home-manager/desktop/nvim/lua/user/plugins/snacks.lua
@@ -1,9 +1,11 @@
 ---@module "lazy"
+---@module "snacks"
 ---@type LazySpec
 return {
   "folke/snacks.nvim",
   -- priority = 1000,
   lazy = false,
+  ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
     -- dashboard = { enabled = true },
