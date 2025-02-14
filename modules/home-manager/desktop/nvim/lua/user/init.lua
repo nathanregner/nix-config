a44505e4e2395610a07ce14d5d7a10a91cdc80bd
Nathan Regner <nathan.regner@clickbank.com>
2025-02-14 09:48:46 -0700

    feat(nvim): disable long_sentences lint

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -323,8 +323,9 @@ require("lazy").setup({
         },
         harper_ls = {
           settings = {
-            ["harper-ls"] = {
+            ["harper-ls"] = { -- https://writewithharper.com/docs/integrations/neovim
               linters = {
+                long_sentences = false,
                 sentence_capitalization = false,
                 spaces = false,
               },
