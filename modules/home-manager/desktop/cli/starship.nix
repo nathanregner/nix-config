4fb1dc6569ac59103711f909724e81644b21d71b
Nathan Regner <nathanregner@gmail.com>
2025-02-17 16:59:15 -0700

    fix(home): avoid IFD

--- modules/home-manager/desktop/cli/starship.nix
+++ modules/home-manager/desktop/cli/starship.nix
@@ -1,33 +1,52 @@
+{ lib, ... }:
 {
-  config,
-  lib,
-  ...
-}:
-let
-  inherit (config.catppuccin) sources;
-  cfg = config.catppuccin.starship;
-in
-{
-  # FIXME: IFD
-  # catppuccin.starship.enable = true;
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
     # https://starship.rs/config
-    settings =
-      {
-        aws.disabled = true;
-        nix_shell = {
-          symbol = "❄️";
-          heuristic = true;
-        };
-        docker_context.only_with_files = false;
-        package.disabled = true;
-      }
-      // {
-        format = lib.mkDefault "$all";
-        palette = "catppuccin_${cfg.flavor}";
-      }
-      // lib.importTOML "${sources.starship.src}/themes/${cfg.flavor}.toml";
+    settings = {
+      aws.disabled = true;
+      nix_shell = {
+        symbol = "❄️";
+        heuristic = true;
+      };
+      docker_context.only_with_files = false;
+      package.disabled = true;
+
+      # FIXME: IFD
+      # catppuccin.starship.enable = false;
+      # nix build .\#homeConfigurations.nregner@enceladus.config.catppuccin.sources.starship.src
+      # nix eval --impure --expr 'builtins.fromTOML (builtins.readFile ./result/themes/mocha.toml)'
+      format = lib.mkDefault "$all";
+      palette = "catppuccin_mocha";
+      palettes.catppuccin_mocha = {
+        base = "#1e1e2e";
+        blue = "#89b4fa";
+        crust = "#11111b";
+        flamingo = "#f2cdcd";
+        green = "#a6e3a1";
+        lavender = "#b4befe";
+        mantle = "#181825";
+        maroon = "#eba0ac";
+        mauve = "#cba6f7";
+        overlay0 = "#6c7086";
+        overlay1 = "#7f849c";
+        overlay2 = "#9399b2";
+        peach = "#fab387";
+        pink = "#f5c2e7";
+        red = "#f38ba8";
+        rosewater = "#f5e0dc";
+        sapphire = "#74c7ec";
+        sky = "#89dceb";
+        subtext0 = "#a6adc8";
+        subtext1 = "#bac2de";
+        surface0 = "#313244";
+        surface1 = "#45475a";
+        surface2 = "#585b70";
+        teal = "#94e2d5";
+        text = "#cdd6f4";
+        yellow = "#f9e2af";
+      };
+    };
   };
 }
