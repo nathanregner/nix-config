c648d5c9c02e4c720eff6873106bcc21c3fdf6c4
Nathan Regner <nathanregner@gmail.com>
2025-02-17 18:18:31 -0700

    wip tests

--- modules/home-manager/desktop/nvim/lua/user/lua/example2_spec.lua
+++ modules/home-manager/desktop/nvim/lua/user/lua/example2_spec.lua
@@ -4,9 +4,27 @@ local autoload = _local_1_["autoload"]
 local _local_2_ = require("plenary.busted")
 local describe = _local_2_["describe"]
 local it = _local_2_["it"]
-local h = autoload("nvim-test.helpers")
 local c = autoload("nfnl.core")
+local h = require("nvim-test.helpers")
+c.assoc(h, "options", {})
+c.assoc(h, "api", {nvim_get_api_info = vim.fn.api_info, nvim_exec_lua = vim.fn.exec_lua})
+h.clear()
 c.keys(vim.g.sexp_filetypes)
 c.merge(c.assoc({}, "a", "b", "c", "asdfasdfasdfasdffa"), c.assoc({}, "d", 1))
---[[ vim.v.lpath (c.keys (require "nvim-test.helpers")) ]]
+--[[ (do h.options) (h.clear) "
+    test
+    test
+    test
+    test
+    test
+    test
+
+    test
+    test
+    test
+    test
+    test
+    test
+    test
+  " (c.keys (require "nvim-test.helpers")) ]]
 return (1 + 2 + 3)
