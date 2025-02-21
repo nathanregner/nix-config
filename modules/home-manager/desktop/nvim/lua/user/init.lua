85f1bd676572c1d5d6cbcc03701b36d2234e6309
Nathan Regner <nathan.regner@clickbank.com>
2025-02-21 15:21:37 -0700

    feat(nvim): add selectioncount to lualine

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -654,7 +654,7 @@ require("lazy").setup({
         lualine_c = { { "filename", path = 1 } },
         lualine_x = { "encoding", "fileformat", "filetype" },
         lualine_y = { "progress" },
-        lualine_z = { "location" },
+        lualine_z = { "location", "selectioncount" },
       },
     },
   },
