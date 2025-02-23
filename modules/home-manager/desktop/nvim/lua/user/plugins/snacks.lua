4e5bf25bbdad77b97fa2144c128ce35950a40fe5
Nathan Regner <nathanregner@gmail.com>
2025-02-23 11:36:20 -0700

    fixup! wip: feat(nvim): add snacks

--- modules/home-manager/desktop/nvim/lua/user/plugins/snacks.lua
+++ modules/home-manager/desktop/nvim/lua/user/plugins/snacks.lua
@@ -121,7 +121,17 @@ return {
     { "<leader>Z", function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
     { "<leader>.", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
     { "<leader>S", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+    { "<leader>N", function() Snacks.notifier.show_history() end, desc = "Notification History" },
     { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+    {
+      "<leader>bD",
+      function()
+        Snacks.bufdelete({
+          filter = function(buf) return #vim.fn.win_findbuf(buf) == 0 end,
+        })
+      end,
+      desc = "Delete Other Buffer",
+    },
     { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
     { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
     { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
@@ -130,23 +140,5 @@ return {
     { "<c-_>", function() Snacks.terminal() end, desc = "which_key_ignore" },
     -- { "]]", function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
     -- { "[[", function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
-    {
-      "<leader>N",
-      desc = "Neovim News",
-      function()
-        Snacks.win({
-          file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-          width = 0.6,
-          height = 0.6,
-          wo = {
-            spell = false,
-            wrap = false,
-            signcolumn = "yes",
-            statuscolumn = " ",
-            conceallevel = 3,
-          },
-        })
-      end,
-    },
   },
 }
