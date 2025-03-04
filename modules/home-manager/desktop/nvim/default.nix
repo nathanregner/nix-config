6d986429de8eba66e201d50be034dd94f2a0d4fd
Nathan Regner <nathan.regner@clickbank.com>
2025-03-04 09:16:57 -0700

    feat(nvim): upgrade graphql-language-service-cli

--- modules/home-manager/desktop/nvim/default.nix
+++ modules/home-manager/desktop/nvim/default.nix
@@ -70,6 +70,7 @@ in
         inherit (pkgs.unstable)
           emmet-language-server
           gopls
+          graphql-language-service-cli
           harper
           helm-ls
           libclang
@@ -80,11 +81,6 @@ in
           vscode-langservers-extracted
           vtsls
           yaml-language-server
-          ;
-
-        # FIXME https://github.com/NixOS/nixpkgs/commit/776868ce712918511e17fe26abcf2a5254a4a3f9
-        inherit (inputs.nixpkgs.legacyPackages.${pkgs.system}.nodejs.pkgs)
-          graphql-language-service-cli
           ;
 
         # formatters/linters
