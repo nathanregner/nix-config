c5f834d2f7a10921fa522c3698ae87b8ed7fbacf
Nathan Regner <nathan.regner@clickbank.com>
2025-03-19 11:48:25 -0600

    Revert "nvim-select"
    This reverts commit 44d7b93bda729d3a9b91d32bb40fc8eecebb36cd.

--- modules/home-manager/desktop/nvim/lua/user/surround.lua
+++ modules/home-manager/desktop/nvim/lua/user/surround.lua
@@ -46,34 +46,3 @@ require("nvim-surround").setup({
     },
   },
 })
-
-local ts_query = require("nvim-treesitter.query")
-local ts_utils = require("nvim-treesitter.ts_utils")
-local utils = require("nvim-surround.utils")
-
----@return user_delete
-function get_selection(bufnr, capture)
-  local matches = ts_query.get_capture_matches_recursively(bufnr, capture, "textobjects")
-  local selections = {}
-  for _, match in ipairs(matches) do
-    local start = match.start
-    local end_ = match.end_
-    if start and start.node and end_ and end_.node then
-      local range = { ts_utils.get_vim_range({ match.node:range() }) }
-      table.insert(selections, {
-        left = {
-          first_pos = { range[1], range[2] },
-          node = start.node,
-        },
-        right = {
-          last_pos = { range[3], range[4] },
-          node = end_.node,
-        },
-      })
-    end
-  end
-  local selection = utils.filter_selections_list(selections)
-  if selection then
-    -- return selection.left.node, selection.right.node
-  end
-end
