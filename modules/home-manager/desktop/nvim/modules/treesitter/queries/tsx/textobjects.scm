4092606588e7da301c23bc00e61d7b4150c082d0
Nathan Regner <nathan.regner@clickbank.com>
2025-03-19 11:48:20 -0600

    nvim-select

--- modules/home-manager/desktop/nvim/modules/treesitter/queries/tsx/textobjects.scm
+++ modules/home-manager/desktop/nvim/modules/treesitter/queries/tsx/textobjects.scm
@@ -1,9 +1,1 @@
-; extends
-
-(jsx_element) @tag.outer
-(jsx_self_closing_element) @tag.outer
-
-(jsx_element
-  open_tag: (_) @_start
-  close_tag: (_) @_end
-  (#make-range-exclusive! "tag.inner" @_start @_end))
+../jsx/textobjects.scm
