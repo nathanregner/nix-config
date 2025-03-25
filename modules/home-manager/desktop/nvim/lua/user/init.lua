597712a6c05b656349398df8b93e5e8d380d25c4
Nathan Regner <nathanregner@gmail.com>
2025-03-24 18:32:52 -0600

    feat(nvim): add neoconf

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -212,6 +212,21 @@ require("lazy").setup({
 
   { "towolf/vim-helm", ft = "helm" },
 
+  {
+    "folke/neoconf.nvim",
+    opts = {
+      plugins = {
+        jsonls = {
+          configured_servers_only = false,
+        },
+      },
+    },
+    config = function(opts)
+      require("neoconf").setup(opts)
+      require("user.neoconf.conform").register()
+    end,
+  },
+
   { -- LSP Configuration & Plugins
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -455,76 +470,8 @@ require("lazy").setup({
 
   { -- Autoformat
     "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        bash = { "shfmt" },
-        clojure = { "joker" },
-        css = { "prettierd" },
-        gitcommit = { "prettier", "injected" }, -- FIXME: prettierd erroring out
-        go = { "gofmt" },
-        graphql = { "prettierd" },
-        html = { "prettierd" },
-        javascript = { "prettierd" },
-        javascriptreact = { "prettierd" },
-        json = { "prettierd" },
-        jsonc = { "prettierd" },
-        lua = { "stylua" },
-        markdown = { "prettierd", "injected" },
-        nginx = { "nginxfmt" },
-        nix = {
-          "nixfmt", --[[ "injected" ]]
-        }, -- FIXME: injected bash formatter broken
-        rust = { "rustfmt" },
-        sh = { "shfmt" },
-        terraform = { "terraform_fmt" },
-        toml = { "taplo" },
-        typescript = { "prettierd" },
-        typescriptreact = { "prettierd" },
-        vue = { "prettierd" },
-        yaml = { "prettierd" },
-        zsh = { "shfmt" },
-
-        -- all filetypes
-        ["*"] = { "trim_whitespace" },
-
-        -- unspecified filetypes
-        ["_"] = { "trim_whitespace" },
-      },
-      formatters = {
-        prettier = { options = { ft_parsers = { gitcommit = "markdown" } } },
-        nginxfmt = {
-          command = "nginxfmt",
-          args = { "--pipe" },
-        },
-      },
-      format_on_save = function(bufnr)
-        -- Disable with a global or buffer-local variable
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
-        return { timeout_ms = 500, lsp_format = "fallback" }
-      end,
-    },
-    config = function(_, opts)
-      require("conform").setup(opts)
-
-      vim.api.nvim_create_user_command("FormatDisable", function(args)
-        if args.bang then
-          vim.g.disable_autoformat = true
-        else
-          ---@diagnostic disable-next-line: inject-field
-          vim.b.disable_autoformat = true
-        end
-      end, {
-        desc = "Disable autoformat-on-save",
-        bang = true,
-      })
-      vim.api.nvim_create_user_command("FormatEnable", function(args)
-        if args.bang then vim.g.disable_autoformat = false end
-        ---@diagnostic disable-next-line: inject-field
-        vim.b.disable_autoformat = false
-      end, {
-        desc = "Re-enable autoformat-on-save",
-      })
-    end,
+    dependencies = { "folke/neoconf.nvim" },
+    config = function() require("user.conform") end,
   },
 
   {
