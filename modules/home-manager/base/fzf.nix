0be20544d2e8c08934db83c54df991f934c8da8f
Nathan Regner <nathan.regner@clickbank.com>
2025-04-03 09:22:58 -0600

    Revert "wip: fzf delete"
    This reverts commit 5e85bb839842aa929f844aa7b8beae65354ac689.

--- modules/home-manager/base/fzf.nix
+++ modules/home-manager/base/fzf.nix
@@ -15,35 +15,18 @@
   };
 
   # https://github.com/junegunn/fzf
-  programs.zsh.initExtraFirst = # bash
-    ''
-      # Use fd (https://github.com/sharkdp/fd) instead of the default find
-      # command for listing path candidates.
-      # - The first argument to the function ($1) is the base path to start traversal
-      # - See the source code (completion.{bash,zsh}) for the details.
-      _fzf_compgen_path() {
-        fd --hidden --follow --exclude ".git" . "$1"
-      }
+  programs.zsh.initExtraFirst = ''
+    # Use fd (https://github.com/sharkdp/fd) instead of the default find
+    # command for listing path candidates.
+    # - The first argument to the function ($1) is the base path to start traversal
+    # - See the source code (completion.{bash,zsh}) for the details.
+    _fzf_compgen_path() {
+      fd --hidden --follow --exclude ".git" . "$1"
+    }
 
-      # Use fd to generate the list for directory completion
-      _fzf_compgen_dir() {
-        fd --type d --hidden --follow --exclude ".git" . "$1"
-      }
-
-      # # https://github.com/junegunn/fzf/discussions/2800#discussioncomment-7931813
-      # export FZF_CTRL_R_OPTS="$(
-      #     cat << EOF
-      #   --bind "ctrl-d:execute(sed -i "" '$d' $HISTFILE)+reload:fc -pa $HISTFILE; fc -rl 1 |
-      #     awk '{ cmd=$0; sub(/^[ \t]*[0-9]+\**[ \t]+/, \"\", cmd); if (!seen[cmd]++) print $0 }'"
-      #   --bind "start:reload:fc -pa $HISTFILE; fc -rl 1 |
-      #     awk '{ cmd=$0; sub(/^[ \t]*[0-9]+\**[ \t]+/, \"\", cmd); if (!seen[cmd]++) print $0 }'"
-      #   --header 'enter select · ^d remove latest'
-      #   --height 100%
-      #   --preview-window "hidden:down:border-top:wrap:<70(hidden)"
-      #   --preview "bat --plain --language sh <<<{2..}"
-      #   --prompt " History > "
-      #   --with-nth 2..
-      #   EOF
-      # )"
-    '';
+    # Use fd to generate the list for directory completion
+    _fzf_compgen_dir() {
+      fd --type d --hidden --follow --exclude ".git" . "$1"
+    }
+  '';
 }
