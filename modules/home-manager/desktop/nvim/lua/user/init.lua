f837a74bd20fd8b0f91afaa8504d58a6065d04a1
Nathan Regner <nathan.regner@clickbank.com>
2025-04-04 10:17:28 -0600

    fix(nvim): disable fidget lazy-loading

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -215,6 +215,7 @@ require("lazy").setup({
 
   { -- Notifications + LSP Progress Messages
     "j-hui/fidget.nvim",
+    lazy = false,
     ---@type fidget.config
     opts = {
       notification = {
