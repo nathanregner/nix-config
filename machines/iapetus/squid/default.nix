62b94cc4a92da637bf916b208988e669e5ac8254
Nathan Regner <nathanregner@gmail.com>
2025-04-05 09:49:24 -0600

    fixup! squid

--- machines/iapetus/squid/default.nix
+++ machines/iapetus/squid/default.nix
@@ -11,8 +11,7 @@
     '';
   };
   # nixpkgs.config.permittedInsecurePackages = [ "squid-7.0.1" ];
-  # https://github.com/NixOS/nixpkgs/issues/27535#issuecomment-317719143
-  systemd.services.nix-daemon.environment = {
-    all_proxy = "http://localhost:3128";
-  };
+  # systemd.services.nix-daemon.environment = {
+  #   all_proxy = "http://localhost:3128";
+  # };
 }
