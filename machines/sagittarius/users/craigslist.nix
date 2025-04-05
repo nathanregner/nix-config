128816cc677f4466b172bc84b64cd20b8b584b94
Nathan Regner <nathanregner@gmail.com>
2025-04-05 16:36:15 -0600

    fix podman on restart?

--- machines/sagittarius/users/craigslist.nix
+++ machines/sagittarius/users/craigslist.nix
@@ -9,6 +9,14 @@
 
   virtualisation.podman.enable = true;
 
+  # https://github.com/containers/podman/discussions/23193
+  environment.etc."tmpfiles.d/podman.conf".text = ''
+    R! /tmp/storage-run-*/containers/
+    R! /tmp/storage-run-*/libpod/tmp/
+    R! /tmp/containers-user-*/containers
+    R! /tmp/podman-run-*/libpod/tmp
+  '';
+
   nginx.subdomain = {
     craigslist."/".proxyPass = "http://127.0.0.1:8888/";
     craigslist-api."/".proxyPass = "http://127.0.0.1:6000/";
