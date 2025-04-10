c82082393d464d0252ab5289b7fdbdaff7503dcc
Nathan Regner <nathan.regner@clickbank.com>
2025-04-10 14:45:59 -0600

    fix(nvim): remove deprecated nvim-ts-autotag options

--- modules/home-manager/desktop/nvim/lua/user/plugins/treesitter.lua
+++ modules/home-manager/desktop/nvim/lua/user/plugins/treesitter.lua
@@ -215,9 +215,6 @@ return {
   },
   {
     "nathanregner/nvim-ts-autotag", -- TODO
-    -- "windwp/nvim-ts-autotag",
     opts = {},
-    -- dev = true,
-    -- dir = "~/dev/github/nvim-ts-autotag",
   },
 }
