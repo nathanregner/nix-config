27603c6b4b86d85a948980e72e2d49dbf760ec4f
Nathan Regner <nathan.regner@clickbank.com>
2025-04-18 11:22:18 -0600

    feat(home): add idea configs

--- modules/home-manager/desktop/jetbrains/default.nix
+++ modules/home-manager/desktop/jetbrains/default.nix
@@ -1,4 +1,67 @@
-{ config, ... }:
 {
-  home.file.".ideavimrc".source = config.lib.file.mkFlakeSymlink ./ideavimrc;
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.programs.jetbrains;
+
+  listFilesRecursive =
+    root:
+    builtins.map (file: {
+      name = (lib.removePrefix "./" (toString (lib.path.removePrefix (root) file)));
+      path = file;
+    }) (lib.filesystem.listFilesRecursive root);
+
+  linkConfigFiles =
+    appName:
+    let
+      commonConfig = (listFilesRecursive ./config/common);
+      appConfig = (listFilesRecursive (./config + "/${appName}"));
+    in
+    builtins.map (
+      { name, path }:
+      {
+        "JetBrains/${appName}/config/${name}" = {
+          source = config.lib.file.mkFlakeSymlink path;
+          force = true;
+        };
+      }
+    ) (commonConfig ++ appConfig);
+in
+{
+  options.programs.jetbrains = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.file.".ideavimrc".source = config.lib.file.mkFlakeSymlink ./ideavimrc;
+
+    home.packages = lib.optionals pkgs.stdenv.isLinux [
+      pkgs.unstable.jetbrains-toolbox
+    ];
+
+    programs.zsh.shellAliases =
+      if pkgs.stdenv.isLinux then
+        {
+          idea = "~/.local/share/JetBrains/Toolbox/apps/intellij-idea-ultimate/bin/idea";
+          datagrip = "~/.local/share/JetBrains/Toolbox/apps/datagrip/bin/datagrip";
+        }
+      else
+        {
+          idea = "open -a /Users/nathan.regner/Applications/IntelliJ\\ IDEA\\ Ultimate\\ *.app";
+          datagrip = "open -a /Users/nathan.regner/Applications/Datagrip\\ *.app";
+        };
+
+    xdg.configFile = lib.mkMerge (
+      builtins.concatMap linkConfigFiles [
+        "idea"
+        "datagrip"
+      ]
+    );
+  };
 }
