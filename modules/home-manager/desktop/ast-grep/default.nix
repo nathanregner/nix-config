207d2c287e4b391a5a40cf331be51e55bf36e6ee
Nathan Regner <nathanregner@gmail.com>
2025-04-18 20:13:15 -0600

    fixup! feat(home): add ast-grep

--- modules/home-manager/desktop/ast-grep/default.nix
+++ modules/home-manager/desktop/ast-grep/default.nix
@@ -1,26 +1,92 @@
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
+  inherit (lib) mkOption types;
+
+  cfg = config.programs.ast-grep;
   yaml = pkgs.formats.yaml { };
+  json = pkgs.formats.json { };
 in
 {
-  home.packages = [
-    pkgs.unstable.ast-grep
-    # pkgs.unstable.tree-sitter
-  ];
+  options.programs.ast-grep = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+    };
 
-  home.file."sgconfig.yml" = {
-    source = yaml.generate "sgconfig.yml" {
-      customLanguages = {
+    ruleDirs = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+    };
+
+    customLanguages = mkOption {
+      type = types.attrsOf (
+        types.submodule {
+          options = {
+            extensions = mkOption {
+              type = types.listOf types.str;
+              example = [ "md" ];
+            };
+            library = mkOption {
+              type = types.package;
+            };
+            expandoChar = mkOption {
+              type = types.nullOr types.str;
+              default = null;
+              example = "_";
+            };
+          };
+        }
+      );
+
+      default = {
         hcl = {
           extensions = [
             "hcl"
             "tf"
           ];
-          libraryPath = "${pkgs.unstable.tree-sitter-grammars.tree-sitter-hcl}/parser";
+          library = pkgs.unstable.tree-sitter-grammars.tree-sitter-hcl;
         };
       };
-      ruleDirs = [ ];
     };
-    force = true;
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs.unstable; [
+      ast-grep
+      tree-sitter
+    ];
+
+    home.file."sgconfig.yml" = {
+      source = yaml.generate "sgconfig.yml" {
+        customLanguages = builtins.mapAttrs (
+          _: lang:
+          {
+            inherit (lang) extensions;
+            libraryPath = "${lang.library}/parser";
+          }
+          // (lib.optionalAttrs (lang.expandoChar != null) {
+            inherit (lang) expandoChar;
+          })
+        ) cfg.customLanguages;
+        inherit (cfg) ruleDirs;
+      };
+    };
+
+    # https://github.com/tree-sitter/tree-sitter/blob/21390af2dd7db090da850ea76ef5ba27d37c41d6/docs/src/cli/init-config.md#parser-directories
+    xdg.configFile."tree-sitter/config.json".source = json.generate "tree-sitter-cli-config.json" {
+      "parser-directories" = [
+        (pkgs.linkFarm "tree-sitter-parser-directory" (
+          lib.mapAttrsToList (name: lang: {
+            inherit name;
+            path = lang.library;
+          }) cfg.customLanguages
+        ))
+      ];
+    };
   };
 }
