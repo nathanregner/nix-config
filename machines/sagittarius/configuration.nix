dbbd0c6a4b328bde15312843faee6e0249efe5a6
Nathan Regner <nathanregner@gmail.com>
2025-04-21 19:59:30 -0600

    feat(sagittarius): enable earlyoom

--- machines/sagittarius/configuration.nix
+++ machines/sagittarius/configuration.nix
@@ -26,6 +26,11 @@
     5001 # iperf
   ];
 
+  services.earlyoom = {
+    enable = true;
+    freeMemThreshold = 5;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
