d12ac994c052cbe17f1fde36ed46f8ef1faf9cb7
Nathan Regner <nathan.regner@clickbank.com>
2025-04-28 09:20:47 -0600

    feat(home): add ff bookmarks

--- modules/home-manager/desktop/firefox/default.nix
+++ modules/home-manager/desktop/firefox/default.nix
@@ -7,8 +7,42 @@
 {
   programs.firefox = {
     enable = true;
+    # TODO: remove once https://github.com/NixOS/nixpkgs/pull/400246
     package = if pkgs.stdenv.isDarwin then null else pkgs.unstable.firefox-devedition-bin;
     # name must start with "dev-edition-"? https://github.com/nix-community/home-manager/issues/4703
+    policies = {
+      ManagedBookmarks = builtins.toJSON [
+        {
+          toplevel_name = "My Managed Bookmarks";
+          children = [
+            {
+              keyword = "hmo";
+              name = "Home-Manager Options";
+              tags = [ "nix" ];
+              url = "https://searchix.alanpearce.eu/options/home-manager/search?query=%s";
+            }
+            {
+              keyword = "dwo";
+              name = "Nix-Darwin Options";
+              tags = [ "nix" ];
+              url = "https://searchix.alanpearce.eu/options/darwin/search?query=%s";
+            }
+            {
+              keyword = "nxo";
+              name = "NixOS Options";
+              tags = [ "nix" ];
+              url = "https://search.nixos.org/options?channel=unstable&query=%s";
+            }
+            {
+              keyword = "nxp";
+              name = "Nix Packages";
+              tags = [ "nix" ];
+              url = "https://search.nixos.org/packages?channel=unstable&query=%s";
+            }
+          ];
+        }
+      ];
+    };
     profiles.dev-edition-default = {
       # FIXME: doesn't auto-install
       extensions.packages = [ pkgs.aws-cli-sso ];
