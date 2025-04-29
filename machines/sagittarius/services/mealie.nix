7654dee6e60ddf3345ae7e1e5a96ff7725d689fe
Nathan Regner <nathanregner@gmail.com>
2025-04-28 18:52:20 -0600

    fix(sagittarius): use unstable mealie module

--- machines/sagittarius/services/mealie.nix
+++ machines/sagittarius/services/mealie.nix
@@ -1,8 +1,17 @@
-{ config, pkgs, ... }:
+{
+  inputs,
+  config,
+  pkgs,
+  ...
+}:
 let
   dataDir = "/var/lib/mealie";
 in
 {
+  disabledModules = [ "services/web-apps/mealie.nix" ];
+  imports = [ "${inputs.nixpkgs-unstable}/nixos/modules/services/web-apps/mealie.nix" ];
+  nixpkgs.overlays = [ (final: prev: { nltk-data = final.unstable.nltk-data; }) ];
+
   services.mealie = {
     enable = true;
     package = pkgs.unstable.mealie;
