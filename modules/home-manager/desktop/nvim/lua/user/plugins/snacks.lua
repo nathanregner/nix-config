506ce797668e0ff1c892285d5d1055f1a2b58e45
Nathan Regner <nathan.regner@clickbank.com>
2025-04-30 14:39:42 -0600

    feat(nvim): add resession.nvim

--- modules/home-manager/desktop/nvim/lua/user/plugins/snacks.lua
+++ modules/home-manager/desktop/nvim/lua/user/plugins/snacks.lua
@@ -118,7 +118,7 @@ return {
     { "<leader>z", function() Snacks.zen() end, desc = "Toggle Zen Mode" },
     { "<leader>Z", function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
     { "<leader>.", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
-    { "<leader>S", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+    -- { "<leader>S", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
     { "<leader>N", function() Snacks.notifier.show_history() end, desc = "Notification History" },
     { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
     {
