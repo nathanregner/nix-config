85356c02a256a7ce413423789cdeba91022eda5d
Nathan Regner <nathan.regner@clickbank.com>
2025-04-30 14:39:42 -0600

    feat(nvim): add resession.nvim

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -1060,6 +1060,44 @@ require("lazy").setup({
     },
   },
 
+  {
+    "stevearc/resession.nvim",
+    config = function()
+      local resession = require("resession")
+      resession.setup({})
+      local function get_session_name()
+        local name = vim.fn.getcwd()
+        local branch = vim.trim(vim.fn.system("git branch --show-current"))
+        if vim.v.shell_error == 0 then
+          return name .. branch
+        else
+          return name
+        end
+      end
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          -- Only load the session if nvim was started with no args
+          if vim.fn.argc(-1) == 0 then
+            resession.load(get_session_name(), { dir = "dirsession", silence_errors = true })
+          end
+        end,
+      })
+      vim.api.nvim_create_autocmd("VimLeavePre", {
+        callback = function() resession.save(get_session_name(), { dir = "dirsession", notify = false }) end,
+      })
+      vim.api.nvim_create_user_command(
+        "Mksession",
+        function() resession.save(get_session_name(), { dir = "dirsession" }) end,
+        {}
+      )
+      vim.api.nvim_create_user_command(
+        "Delsession",
+        function() resession.delete(get_session_name(), { dir = "dirsession" }) end,
+        {}
+      )
+    end,
+  },
+
   { import = "user.plugins" },
 }, {
   dev = {
