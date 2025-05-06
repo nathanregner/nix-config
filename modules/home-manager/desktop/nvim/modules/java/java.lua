869f5b627550f7d2e74f3aad348a8bfc580e763b
Nathan Regner <nathan.regner@clickbank.com>
2025-05-06 16:38:26 -0600

    feat(nvim): enable jdtls rename file rename capabilities

--- modules/home-manager/desktop/nvim/modules/java/java.lua
+++ modules/home-manager/desktop/nvim/modules/java/java.lua
@@ -17,5 +17,10 @@ local config = {
   flags = {
     debounce_text_changes = 250,
   },
+  capabilities = vim.tbl_deep_extend(
+    "force",
+    vim.lsp.protocol.make_client_capabilities(),
+    require("lsp-file-operations").default_capabilities()
+  ),
 }
 require("jdtls").start_or_attach(config)
