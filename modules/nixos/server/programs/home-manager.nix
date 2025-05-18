29783491023e90cde5f0f52ae616466f86495b4c
Nathan Regner <nathanregner@gmail.com>
2025-05-18 11:58:38 -0600

    fix(nixos): add /share/zsh to pathsToLink

--- modules/nixos/server/programs/home-manager.nix
+++ modules/nixos/server/programs/home-manager.nix
@@ -14,9 +14,10 @@
     enable = lib.mkEnableOption "Enable minimal home-manager profile for server usage";
   };
 
-  config = lib.mkIf config.programs.nregner.home-manager.enable {
+  config = lib.mkIf config.local.programs.home-manager.enable {
     programs.zsh.enable = true;
     users.users.nregner.shell = pkgs.zsh;
+    environment.pathsToLink = [ "/share/zsh" ]; # as required by home-manager
 
     home-manager = {
       useGlobalPkgs = true;
