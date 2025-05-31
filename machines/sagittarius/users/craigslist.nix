85c73568aad5805fdc5b43c6c7c11e84cbeabb15
Nathan Regner <nathanregner@gmail.com>
2025-05-30 20:22:52 -0600

    feat(sagittarius): backup user directories

--- machines/sagittarius/users/craigslist.nix
+++ machines/sagittarius/users/craigslist.nix
@@ -1,11 +1,5 @@
-{ self, lib, ... }:
 {
-  users.users.craigslist = {
-    isNormalUser = true;
-    extraGroups = [ "docker" ];
-    openssh.authorizedKeys.keys = builtins.attrValues self.globals.ssh.userKeys.nregner;
-    linger = true;
-  };
+  local.users.craigslist = { };
 
   virtualisation.podman.enable = true;
 
@@ -21,9 +15,4 @@
     craigslist."/".proxyPass = "http://127.0.0.1:8888/";
     craigslist-api."/".proxyPass = "http://127.0.0.1:6000/";
   };
-
-  # https://discourse.nixos.org/t/nixos-rebuild-switch-is-failing-when-systemd-linger-is-enabled/31937/5
-  systemd.user.services.nixos-activation.unitConfig.ConditionUser = lib.mkForce [
-    "!craigslist"
-  ];
 }
--- machines/sagittarius/users/minecraft.nix
+++ machines/sagittarius/users/minecraft.nix
@@ -1,27 +1,16 @@
-{ self, lib, ... }:
 {
-  users.users.minecraft = {
-    isNormalUser = true;
-    openssh.authorizedKeys.keys = builtins.attrValues self.globals.ssh.userKeys.nregner;
-    linger = true;
-  };
+  local.users.minecraft = { };
 
   networking.firewall = {
     allowedTCPPorts = [ 25565 ];
     allowedUDPPorts = [ 25565 ];
   };
 
-  # https://discourse.nixos.org/t/nixos-rebuild-switch-is-failing-when-systemd-linger-is-enabled/31937/5
-  systemd.user.services.nixos-activation.unitConfig.ConditionUser = lib.mkForce [
-    "!minecraft"
-  ];
-
   nginx.subdomain.minecraft."/" = {
-    extraConfig = # nginx
-      ''
-        alias "/home/minecraft/www/";
-        autoindex on;
-      '';
+    extraConfig = ''
+      alias "/home/minecraft/www/";
+      autoindex on;
+    '';
   };
 
   # https://www.freedesktop.org/software/systemd/man/latest/systemd.exec.html
