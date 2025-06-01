eda4fe834a583bf1af8f51e28e469b9ef73d85a9
Nathan Regner <nathanregner@gmail.com>
2025-06-01 10:26:59 -0600

    WIP(nvim): add more sortables

--- modules/home-manager/desktop/nvim/lua/user/plugins/treesitter.lua
+++ modules/home-manager/desktop/nvim/lua/user/plugins/treesitter.lua
@@ -163,9 +163,8 @@ return {
           shorthand = { node = "shorthand_property_identifier" },
         },
         nix = {
-          -- TODO: inherit(a) b c d;
-          -- attr = { node = { "attr" } },
-          attrset = { node = { "binding" } },
+          -- attr = { node = { "identifier" } }, -- inherit(a) b c d;
+          binding = { node = { "binding" }, ordinal = { "attrpath" } }, -- let, attrset
           formal = {
             node = { "formal" },
             order_by = function(node1, node2)
@@ -176,6 +175,8 @@ return {
                 "self",
                 "inputs",
                 "inputs'",
+                "outputs",
+                "outputs'",
                 "sources",
                 "config",
                 "pkgs",
