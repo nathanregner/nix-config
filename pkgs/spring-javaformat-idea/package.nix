22bee2117d6cfc2645d9845b5df8b233f8a41e54
Nathan Regner <nathan.regner@clickbank.com>
2025-06-11 16:18:18 -0600

    fixup! up spring-javaformat

--- pkgs/spring-javaformat-idea/package.nix
+++ pkgs/spring-javaformat-idea/package.nix
@@ -1,14 +1,17 @@
 {
   fetchurl,
-  srcOnly,
   stdenvNoCC,
 }:
-srcOnly rec {
+stdenvNoCC.mkDerivation rec {
   name = "spring-javaformat-intellij-idea-plugin";
   version = "0.0.46";
   src = fetchurl {
     url = "https://repo1.maven.org/maven2/io/spring/javaformat/spring-javaformat-intellij-idea-plugin/${version}/spring-javaformat-intellij-idea-plugin-${version}.jar";
     hash = "sha256-E8OA03TAPuVoioXxuDHTkpnGexWeqDfBW1nBT+1PeCE=";
   };
-  stdenv = stdenvNoCC;
+  dontUnpack = true;
+
+  installPhase = ''
+    cp $src $out
+  '';
 }
