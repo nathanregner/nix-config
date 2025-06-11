4e54cb8c7211d2eeca02d82ab6b40a69cad175cf
Nathan Regner <nathanregner@gmail.com>
2025-06-10 22:41:35 -0600

    revert: "fix(nvim): disable nil_ls"
    This reverts commit 60e2379498ed472abefade813250d8aef088dcad.

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -441,7 +441,7 @@ require("lazy").setup({
             -- diagnostics = { disable = { 'missing-fields' } },
           },
         },
-        -- nil_ls = {},
+        nil_ls = {},
         nixd = {},
         nushell = {},
         omnisharp = {
