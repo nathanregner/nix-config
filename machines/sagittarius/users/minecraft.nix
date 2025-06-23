18f5d498947f574383322869d62e0388a677d496
Nathan Regner <nathanregner@gmail.com>
2025-06-23 16:30:46 -0600

    feat(sagittarius): increase backup frequency of user dirs

--- machines/sagittarius/users/minecraft.nix
+++ machines/sagittarius/users/minecraft.nix
@@ -1,5 +1,19 @@
+{ lib, ... }:
 {
   local.users.minecraft = { };
+
+  # TODO: cleanup
+  services.restic.backups.minecraft-s3 = {
+    timerConfig = {
+      OnCalendar = lib.mkForce "hourly";
+      Persistent = true;
+    };
+    # TODO: this option should not merge
+    pruneOpts = lib.mkForce [
+      "--keep-within 1d"
+      "--keep-within-daily 1m"
+    ];
+  };
 
   networking.firewall = {
     allowedTCPPorts = [ 25565 ];
