0d310f5b45d53679a20e1d32c2718d44451655d0
Nathan Regner <nathan.regner@clickbank.com>
2025-07-03 16:45:56 -0600

    fixup! feat(nvim): replace ultimate-autopair with nvim-autopairs

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -132,34 +132,7 @@ require("lazy").setup({
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    config = function()
-      local npairs = require("nvim-autopairs")
-      local Rule = require("nvim-autopairs.rule")
-      local cond = require("nvim-autopairs.conds")
-
-      npairs.setup({
-        disabled_filetype = { "TelescopePrompt", "snacks_picker_input" },
-        check_ts = true,
-        break_undo = true,
-      })
-
-      -- https://github.com/windwp/nvim-autopairs/wiki/Custom-rules
-
-      -- lisp quoting
-      table.insert(npairs.get_rules("'")[1].not_filetypes, { "clojure", "lisp" })
-
-      -- generics
-      npairs.add_rule(Rule("<", ">", {
-        "-html",
-        "-javascriptreact",
-        "-typescriptreact",
-        "-xml",
-      }):with_pair(
-        -- regex will make it so that it will auto-pair on `a<` but not `a <`
-        -- The `:?:?` part makes it also work on Rust generics like `some_func::<T>()`
-        cond.before_regex("%a+:?:?$", 3)
-      ):with_move(function(opts) return opts.char == ">" end))
-    end,
+    config = function() require("user.pairs") end,
   },
 
   {
