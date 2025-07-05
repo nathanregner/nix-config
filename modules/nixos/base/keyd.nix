eb9134e1ab1a1e0e2b8386148ba22f84a7a82791
Nathan Regner <nathanregner@gmail.com>
2025-07-05 12:05:05 -0600

    feat(nixos): remap caps-n, caps-p

--- modules/nixos/base/keyd.nix
+++ modules/nixos/base/keyd.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   services.keyd = {
     enable = true;
@@ -7,6 +8,17 @@
         capslock = "overload(control, esc)"; # remap to escape when pressed and control when held
         esc = "capslock";
       };
+      control = {
+        n = "down";
+        p = "up";
+      };
     };
   };
+
+  # TOOD
+  # systemd.services.keyd.restartTriggers = [ ];
+
+  environment.systemPackages = [
+    pkgs.keyd
+  ];
 }
