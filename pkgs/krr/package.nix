d05c0547e71b45129ff9eeacd44dc45bafddf5ed
Nathan Regner <nathan.regner@clickbank.com>
2025-07-11 15:32:42 -0600

    build: update inputs

--- pkgs/krr/package.nix
+++ pkgs/krr/package.nix
@@ -1,48 +1,64 @@
 {
   inputs,
-  pkgs,
-  nix-update-script,
+  lib,
+  callPackage,
+  fetchFromGitHub,
+  python311,
+  srcOnly,
+  stdenv,
   ...
 }:
 let
-  inherit (inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; })
-    defaultPoetryOverrides
-    mkPoetryApplication
-    ;
-  pypkgs-build-requirements = {
-    about-time = [ "setuptools" ];
-    alive-progress = [ "setuptools" ];
-    prometheus-api-client = [ "setuptools" ];
+  python = python311;
+
+  workspace = inputs.uv2nix.lib.workspace.loadWorkspace {
+    workspaceRoot = srcOnly rec {
+      pname = "krr";
+      version = "1.24.0";
+      src = fetchFromGitHub {
+        owner = "robusta-dev";
+        repo = "krr";
+        rev = "v${version}";
+        sha256 = "sha256-2Kj94Co+4JV/ikLBUFqV4BdwFJSzvsbchf6As9U7LpQ=";
+      };
+      # uvx migrate-to-uv
+      patches = [ ./uv.patch ];
+      inherit stdenv;
+    };
   };
-  overrides = defaultPoetryOverrides.extend (
-    _final: prev:
-    builtins.mapAttrs (
-      package: build-requirements:
-      (builtins.getAttr package prev).overridePythonAttrs (old: {
-        buildInputs =
-          (old.buildInputs or [ ])
-          ++ (builtins.map (
-            pkg: if builtins.isString pkg then builtins.getAttr pkg prev else pkg
-          ) build-requirements);
-      })
-    ) pypkgs-build-requirements
-  );
+
+  # Create package overlay from workspace.
+  overlay = workspace.mkPyprojectOverlay {
+    # Prefer prebuilt binary wheels as a package source.
+    # Sdists are less likely to "just work" because of the metadata missing from uv.lock.
+    # Binary wheels are more likely to, but may still require overrides for library dependencies.
+    sourcePreference = "wheel"; # or sourcePreference = "sdist";
+    # Optionally customise PEP 508 environment
+    # environ = {
+    #   platform_release = "5.10.65";
+    # };
+  };
+
+  pyprojectOverrides = _final: _prev: {
+    # Implement build fixups here.
+    # Note that uv2nix is _not_ using Nixpkgs buildPythonPackage.
+    # It's using https://pyproject-nix.github.io/pyproject.nix/build.html
+    grapheme = python.pkgs.grapheme;
+  };
+
+  pythonSet =
+    # Use base package set from pyproject.nix builders
+    (callPackage inputs.pyproject-nix.build.packages { inherit python; }).overrideScope (
+      lib.composeManyExtensions [
+        inputs.pyproject-build-systems.overlays.default
+        overlay
+        pyprojectOverrides
+      ]
+    );
+
+  inherit (callPackage inputs.pyproject-nix.build.util { }) mkApplication;
 in
-mkPoetryApplication {
-  # TODO: nvfetcher
-  projectDir = pkgs.fetchFromGitHub {
-    owner = "robusta-dev";
-    repo = "krr";
-    rev = "v1.16.0";
-    fetchSubmodules = false;
-    sha256 = "sha256-jPTSp/IBhhWzRg0qjakE7HOCgXAcgFJejePVRFIzAs0=";
-  };
-  inherit overrides;
-
-  dontCheckRuntimeDeps = true;
-  doCheck = false;
-
-  passthru = {
-    updateScript = nix-update-script { };
-  };
+mkApplication {
+  venv = pythonSet.mkVirtualEnv "application-env" workspace.deps.default;
+  package = pythonSet.robusta-krr;
 }
