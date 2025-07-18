48f0702048c9fd6cc179674b0c8dba63101333ef
Nathan Regner <nathanregner@gmail.com>
2025-07-18 17:06:43 -0600

    build: update packages

--- pkgs/node-exporter-full/package.nix
+++ pkgs/node-exporter-full/package.nix
@@ -5,12 +5,12 @@
 }:
 stdenvNoCC.mkDerivation {
   pname = "node-exporter-full.json";
-  version = "0-unstable-2025-07-18";
+  version = "0-unstable-2025-07-23";
   src = fetchFromGitHub {
     owner = "rfmoz";
     repo = "grafana-dashboards";
-    rev = "57d5dd34990b638032c6fab104f368f970888751";
-    sha256 = "sha256-OVV/KG02pgPXqqvkvqDVzx44jR2vnQ6e3c8b7Ew/c3Y=";
+    rev = "925c74c8af03133a5c1969a73b5af8ef4fe1c488";
+    sha256 = "sha256-SiYaSNgXGFyEJaL0xbD8rXBcetl3P5ylA0CwOD4cJbU=";
   };
 
   installPhase = ''
