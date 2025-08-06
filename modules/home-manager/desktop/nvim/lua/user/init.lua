2e3bde8dde3065ac4eb7f1ef5365d97b51871b56
Nathan Regner <nathan.regner@clickbank.com>
2025-08-06 10:50:16 -0600

    feat(nvim): automatically set filetype for .env files

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -1317,6 +1317,28 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   end,
 })
 
+-- https://neovim.discourse.group/t/how-to-add-custom-filetype-detection-to-various-env-files/4272/3
+vim.filetype.add({
+  -- Detect and assign filetype based on the extension of the filename
+  extension = {
+    mdx = "mdx",
+    log = "log",
+    conf = "conf",
+    env = "sh",
+  },
+  -- Detect and apply filetypes based on the entire filename
+  filename = {
+    [".env"] = "sh",
+    ["env"] = "sh",
+    ["tsconfig.json"] = "jsonc",
+  },
+  -- Detect and apply filetypes based on certain patterns of the filenames
+  pattern = {
+    -- INFO: Match filenames like - ".env.example", ".env.local" and so on
+    ["%.env%.[%w_.-]+"] = "sh",
+  },
+})
+
 -- for GBrowse, now that netrw is disabled
 vim.api.nvim_create_user_command(
   "Browse",
