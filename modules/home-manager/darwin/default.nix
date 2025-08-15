dfe09d03bac1039a420efb0b629cbad90a7c5ef9
Nathan Regner <nathan.regner@clickbank.com>
2025-08-15 15:00:14 -0600

    fix(darwin): disable automatic gc/optimization
    Seems to result in high `syspolicyd` CPU usage and system-wide freezes.

--- modules/home-manager/darwin/default.nix
+++ modules/home-manager/darwin/default.nix
@@ -1,9 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   imports = [
     ./hammerspoon.nix
     ./scroll-reverser.nix
   ];
+
+  # https://github.com/nix-darwin/nix-darwin/issues/1307
+  nix.gc.automatic = lib.mkForce false;
 
   # prefer these over system utilities for consistency with linux
   home.packages = with pkgs.unstable; [
