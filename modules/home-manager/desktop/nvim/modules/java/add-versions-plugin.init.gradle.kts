d9cd1b25bbe2c10bbb0f61e4b0b54ae174d208e7
Nathan Regner <nathanregner@gmail.com>
2025-08-16 12:49:30 -0600

    feat(home): configure gradle-versions-plugin filters

--- modules/home-manager/desktop/nvim/modules/java/add-versions-plugin.init.gradle.kts
+++ modules/home-manager/desktop/nvim/modules/java/add-versions-plugin.init.gradle.kts
@@ -9,10 +9,20 @@ initscript {
   }
 }
 
+val STABLE_VERSION = "^[0-9,.v-]+(-r)?$".toRegex()
+
+fun isStable(version: String): Boolean {
+  val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+  val stableVersion = STABLE_VERSION.matches(version)
+  return stableKeyword || stableVersion
+}
+
 allprojects {
   apply<com.github.benmanes.gradle.versions.VersionsPlugin>()
 
   tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
-    // configure the task, for example wrt. resolution strategies
+    rejectVersionIf {
+      isStable(currentVersion) && !isStable(candidate.version)
+    }
   }
 }
