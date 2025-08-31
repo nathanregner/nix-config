8079c2b5dbbfba793c7a4f1b3507e0a1f6a99a57
Nathan Regner <nathanregner@gmail.com>
2025-08-31 08:31:00 -0600

    refactor(home): move deprecated ssh options

--- modules/home-manager/base/default.nix
+++ modules/home-manager/base/default.nix
@@ -11,13 +11,14 @@
   config = {
     programs.ssh = {
       enable = true;
-      # https://docs.ssh.com/manuals/server-zos-user/64/disabling-agent-forwarding.html
-      forwardAgent = false;
-      # share connections
-      controlMaster = "auto";
-      controlPersist = "10m";
+      enableDefaultConfig = false;
       matchBlocks."*" = {
         sendEnv = [ "TMUX" ];
+        # https://docs.ssh.com/manuals/server-zos-user/64/disabling-agent-forwarding.html
+        forwardAgent = false;
+        # share connections
+        controlMaster = "auto";
+        controlPersist = "10m";
       };
     };
 
@@ -29,7 +30,7 @@
     nix.gc = {
       automatic = true;
       options = "--delete-older-than 7d";
-      frequency = "weekly";
+      dates = "weekly";
     };
   };
 }
