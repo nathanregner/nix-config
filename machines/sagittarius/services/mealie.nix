e6aced4fa85d8135dd1c9d22001130ba781e984e
Nathan Regner <nathanregner@gmail.com>
2025-09-08 19:52:56 -0600

    feat(sagittarius): switch to mealie unstable

--- machines/sagittarius/services/mealie.nix
+++ machines/sagittarius/services/mealie.nix
@@ -1,4 +1,5 @@
 {
+  inputs,
   config,
   pkgs,
   ...
@@ -7,9 +8,13 @@ let
   dataDir = "/var/lib/mealie";
 in
 {
+  disabledModules = [ "services/web-apps/mealie.nix" ];
+  imports = [ "${inputs.nixpkgs-unstable}/nixos/modules/services/web-apps/mealie.nix" ];
+  nixpkgs.overlays = [ (final: prev: { nltk-data = final.unstable.nltk-data; }) ];
+
   services.mealie = {
     enable = true;
-    package = pkgs.mealie;
+    package = pkgs.unstable.mealie;
     port = 9000;
   };
 
