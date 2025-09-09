449d1d75485a165757684ab74a013fc12750239c
Nathan Regner <nathanregner@gmail.com>
2025-09-08 20:03:13 -0600

    feat(home): sync easyeffects settings

--- modules/home-manager/desktop/linux/easyeffects.nix
+++ modules/home-manager/desktop/linux/easyeffects.nix
@@ -3,6 +3,17 @@
 
   # Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
   dconf.settings = {
+    "com/github/wwmm/easyeffects/streamoutputs" = {
+      blocklist = [
+        "WEBRTC VoiceEngine"
+        "win10"
+      ];
+      output-device = "alsa_output.usb-SteelSeries_Arctis_Nova_7-00.analog-stereo";
+      plugins = [
+        "bass_enhancer#0"
+      ];
+    };
+
     "com/github/wwmm/easyeffects/streamoutputs/bassenhancer/0" = {
       amount = 6.0;
       bypass = false;
