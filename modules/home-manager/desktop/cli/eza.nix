6aef90ccd422dcb04323947d96f07c1e94324510
Nathan Regner <nathan.regner@clickbank.com>
2025-10-20 09:18:59 -0600

    fix(home): eza config on darwin

--- modules/home-manager/desktop/cli/eza.nix
+++ modules/home-manager/desktop/cli/eza.nix
@@ -1,7 +1,18 @@
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
   programs.eza = {
     enable = true;
     enableZshIntegration = true;
   };
   catppuccin.eza.enable = true;
+
+  # lazy fix for mismatched config path on darwin
+  home.file."Library/Application Support/eza".source = lib.mkIf pkgs.stdenv.isDarwin (
+    config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/eza"
+  );
 }
