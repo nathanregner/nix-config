0e73c8825a85f9c8c91b39c6eedac21eb20688f9
Nathan Regner <nathan.regner@clickbank.com>
2025-11-17 10:17:31 -0700

    feat(nvim): add enable options to all language modules

--- modules/home-manager/desktop/nvim/modules/java/default.nix
+++ modules/home-manager/desktop/nvim/modules/java/default.nix
@@ -12,7 +12,7 @@ in
   options.programs.neovim.modules.java = {
     enable = mkOption {
       type = types.bool;
-      default = false;
+      default = true;
     };
 
     finalPackage = mkOption {
