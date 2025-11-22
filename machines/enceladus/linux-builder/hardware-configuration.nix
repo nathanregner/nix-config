001614c00ef5fe4e080ac6ea1da0ee974f3c830c
Nathan Regner <nathanregner@gmail.com>
2025-11-22 10:54:57 -0700

    WIP: fix(darwin): attempt to fix VM clock desync

--- machines/enceladus/linux-builder/hardware-configuration.nix
+++ machines/enceladus/linux-builder/hardware-configuration.nix
@@ -1,9 +1,14 @@
 { lib, ... }:
 {
   nixpkgs.hostPlatform = "aarch64-linux";
-
   virtualisation = {
     cores = 8; # TODO: Figure out why this can't be > 8
     diskSize = lib.mkForce (64 * 1024);
+
+    # https://www.reddit.com/r/qemu_kvm/comments/lboxit/time_sync_macos_host_linux_guest
+    qemu.options = [
+      "-rtc"
+      "base=localtime,clock=host"
+    ];
   };
 }
