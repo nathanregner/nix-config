6f8c92acc6655494a43a0205117651427e8c3a2f
Nathan Regner <nathanregner@gmail.com>
2025-12-26 23:36:11 -0700

    fixup! WIP: refactor

--- modules/nixos/base/services/backups.nix
+++ modules/nixos/base/services/backups.nix
@@ -7,6 +7,35 @@
 with lib;
 let
   cfg = config.local.services.backup;
+  baseOptions = options.services.restic.backups.type.getSubOptions [ ];
+  mkDefault = default: mkOption { inherit default; };
+  mkReadonly =
+    default:
+    mkOption {
+      inherit default;
+      readOnly = true;
+    };
+  mkTarget =
+    options:
+    mkOption {
+      type = types.submodule {
+        options = {
+          enable = mkOption {
+            type = types.bool;
+            default = true;
+          };
+        }
+        // options
+        // builtins.getAttrs baseOptions (
+          builtins.attrNames (lib.filterAttrs (_name: option: !option.readOnly) options)
+          ++ [
+            "pruneOpts"
+            "timerConfig"
+          ]
+        );
+      };
+    };
+
 in
 {
   options.local.services.backup.enable = mkOption {
@@ -25,51 +54,36 @@ in
               "dynamicFilesFrom"
               "paths"
               "timerConfig"
-            ] (options.services.restic.backups.type.getSubOptions [ ]))
+            ] baseOptions)
             // {
               restic = mkOption {
                 type = types.submodule {
+                  default = { };
                   options = {
-                    s3 =
-
-                      let
-                        s3Defaults = {
-                          repository = "s3:s3.dualstack.us-west-2.amazonaws.com/nregner-restic/${config.networking.hostName}/${name}";
-                          initialize = true;
-                          passwordFile = config.sops.secrets.restic-password.path;
-                          environmentFile = config.sops.secrets.restic-s3-env.path;
-                          pruneOpts = [
-                            "--keep-within 7d"
-                            "--keep-within-daily 1m"
-                            "--keep-within-weekly 6m"
-                            "--keep-within-monthly 1y"
-                          ];
-                        };
-                      in
-                      mkOption {
-                        type = types.submodule {
-                          options = {
-                            enable = mkOption {
-                              type = types.bool;
-                              default = true;
-                            };
-                          }
-                          // builtins.mapAttrs (
-                            _name: value:
-                            mkOption {
-                              readOnly = true;
-                              default = value;
-                            }
-                          ) s3Defaults
-                          // builtins.removeAttrs (options.services.restic.backups.type.getSubOptions [ ]) (
-                            [
-                              "enable"
-                              "_module"
-                            ]
-                            ++ builtins.attrNames s3Defaults
-                          );
-                        };
-                      };
+                    s3 = mkTarget {
+                      repository = mkReadonly "s3:s3.dualstack.us-west-2.amazonaws.com/nregner-restic/${config.networking.hostName}/${name}";
+                      initialize = mkReadonly true;
+                      passwordFile = mkReadonly config.sops.secrets.restic-password.path;
+                      environmentFile = mkReadonly config.sops.secrets.restic-s3-env.path;
+                      pruneOpts = mkDefault [
+                        "--keep-within 7d"
+                        "--keep-within-daily 1m"
+                        "--keep-within-weekly 6m"
+                        "--keep-within-monthly 1y"
+                      ];
+                    };
+                    hdd = mkTarget {
+                      repository = mkReadonly "/vol/backup/restic/${name}";
+                      initialize = mkReadonly true;
+                      passwordFile = mkReadonly config.sops.secrets.restic-password.path;
+                      environmentFile = mkReadonly config.sops.secrets.restic-s3-env.path;
+                      pruneOpts = mkDefault [
+                        "--keep-within 7d"
+                        "--keep-within-daily 1m"
+                        "--keep-within-weekly 6m"
+                        "--keep-within-monthly 1y"
+                      ];
+                    };
                   };
                 };
               };
@@ -84,9 +98,6 @@ in
       # https://discourse.nixos.org/t/psa-pinning-users-uid-is-important-when-reinstalling-nixos-restoring-backups/21819
       local.services.backup.paths.nixos = {
         paths = [ "/var/lib/nixos" ];
-        restic = {
-          s3 = { };
-        };
       };
     }
     (lib.mkIf cfg.enable (
