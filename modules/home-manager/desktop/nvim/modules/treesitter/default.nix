89b78cfdbca3e7001b68e0498827023461404da5
Nathan Regner <nathanregner@gmail.com>
2026-01-02 10:16:06 -0700

    build: update flake.lock

--- modules/home-manager/desktop/nvim/modules/treesitter/default.nix
+++ modules/home-manager/desktop/nvim/modules/treesitter/default.nix
@@ -1,18 +1,11 @@
 {
-  pkgs,
   config,
+  pkgs,
+  lib,
   ...
 }:
 let
   parserPrefix = "nvim/nvim-treesitter";
-  package = pkgs.unstable.vimPlugins.nvim-treesitter.withAllGrammars.overrideAttrs (old: {
-    patches = old.patches or [ ] ++ [
-      (pkgs.fetchpatch {
-        url = "https://github.com/nvim-treesitter/nvim-treesitter/pull/7742/commits/fbcafd3e51200b3788652aef90147caade380750.patch";
-        sha256 = "sha256-wOScAN6QqTW18HskDyNLbeg8Zgf5WNLYu39Hef0TQj8=";
-      })
-    ];
-  });
 in
 {
   programs.neovim.lua.globals =
@@ -21,7 +14,7 @@ in
     in
     {
       nvim_treesitter = {
-        dir = "${package}";
+        dir = "${pkgs.unstable.vimPlugins.nvim-treesitter.withAllGrammars}";
         inherit parser_install_dir;
       };
       rtp = [ parser_install_dir ];
@@ -32,21 +25,13 @@ in
     force = true;
   };
 
-  xdg.dataFile = builtins.listToAttrs (
-    builtins.map (
-      grammar:
-      let
-        language = builtins.elemAt (builtins.match "vimplugin-treesitter-grammar-(.*)" grammar.name) 0;
-      in
-      {
-        name = "${parserPrefix}/parser/${language}.so";
-        value = {
-          source = "${grammar}/parser/${language}.so";
-          force = true;
-        };
-      }
-    ) package.passthru.dependencies
-  );
+  xdg.dataFile = lib.mapAttrs' (name: grammar: {
+    name = "${parserPrefix}/parser/${name}.so";
+    value = {
+      source = "${grammar}/parser";
+      force = true;
+    };
+  }) pkgs.unstable.vimPlugins.nvim-treesitter.passthru.builtGrammars;
 
   programs.topiary.languages.tree_sitter_query = {
     extensions = [ "scm" ];
