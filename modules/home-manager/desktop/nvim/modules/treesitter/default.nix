82fee5903864f84a3c53b025d9ed228ec0b5bd0a
Nathan Regner <nathanregner@gmail.com>
2026-01-31 14:04:28 -0700

    fix(nvim): use nvim-treesitter latest; compile grammars as needed

--- modules/home-manager/desktop/nvim/modules/treesitter/default.nix
+++ modules/home-manager/desktop/nvim/modules/treesitter/default.nix
@@ -5,59 +5,103 @@
   ...
 }:
 let
+  cfg = config.programs.neovim.treesitter;
+  package = pkgs.unstable.vimPlugins.nvim-treesitter.withAllGrammars;
   parserPrefix = "nvim/nvim-treesitter";
 in
 {
-  programs.neovim.lua.globals =
-    let
-      parser_install_dir = "${config.xdg.dataHome}/${parserPrefix}";
-    in
-    {
-      nvim-treesitter = {
-        dir = "${pkgs.unstable.vimPlugins.nvim-treesitter.withAllGrammars}";
-        inherit parser_install_dir;
-      };
-      rtp = [ parser_install_dir ];
+  options.programs.neovim.treesitter = {
+    grammarSource = lib.mkOption {
+      type = lib.types.enum [
+        "nix"
+        "git"
+      ];
+      default = "nix";
     };
-
-  xdg.configFile."nvim/after/queries" = {
-    source = config.lib.file.mkFlakeSymlink ./queries;
-    force = true;
   };
 
-  xdg.dataFile = lib.mapAttrs' (name: grammar: {
-    name = "${parserPrefix}/parser/${name}.so";
-    value = {
-      source = "${grammar}/parser";
+  config = {
+    programs.neovim.lua.globals = lib.optionalAttrs (cfg.grammarSource == "nix") (
+      let
+        parser_install_dir = "${config.xdg.dataHome}/${parserPrefix}";
+      in
+      {
+        nvim-treesitter = {
+          dir = "${package}";
+          inherit parser_install_dir;
+        };
+        rtp = [ parser_install_dir ];
+      }
+    );
+
+    # :checkhealth nvim-treesitter
+    home.packages = lib.optionals (cfg.grammarSource == "git") (
+      with pkgs.unstable;
+      [
+        curl
+        gnumake
+        gnutar
+        stdenv.cc
+        tree-sitter-latest
+      ]
+    );
+
+    xdg.configFile."nvim/after/queries" = {
+      source = config.lib.file.mkFlakeSymlink ./queries;
       force = true;
     };
-  }) pkgs.unstable.vimPlugins.nvim-treesitter.passthru.builtGrammars;
 
-  programs.topiary.languages.tree_sitter_query = {
-    extensions = [ "scm" ];
-    grammar = {
-      package = pkgs.tree-sitter-grammars.tree-sitter-query;
-      symbol = "tree_sitter_query";
+    xdg.dataFile = lib.optionalAttrs (cfg.grammarSource == "nix") (
+      lib.listToAttrs (
+        builtins.filter (grammar: grammar ? name) (
+          map (
+            dependency:
+            let
+              match = builtins.match "nvim-treesitter-grammar-(.*)" (dependency.pname or "");
+            in
+            lib.optionalAttrs (match != null) (
+              let
+                language = builtins.elemAt match 0;
+              in
+              {
+                name = "${parserPrefix}/parser/${language}.so";
+                value = {
+                  source = "${dependency}/parser/${language}.so";
+                  force = true;
+                };
+              }
+            )
+          ) package.passthru.dependencies
+        )
+      )
+    );
+
+    programs.topiary.languages.tree_sitter_query = {
+      extensions = [ "scm" ];
+      grammar = {
+        package = pkgs.tree-sitter-grammars.tree-sitter-query;
+        symbol = "tree_sitter_query";
+      };
     };
+
+    programs.ast-grep.customLanguages =
+      let
+        parsers = pkgs.vimPlugins.nvim-treesitter.passthru.builtGrammars;
+      in
+      {
+        terraform = {
+          extensions = [
+            "hcl"
+            "tf"
+          ];
+          library = parsers.terraform;
+        };
+        xml = {
+          extensions = [
+            "xml"
+          ];
+          library = parsers.xml;
+        };
+      };
   };
-
-  programs.ast-grep.customLanguages =
-    let
-      parsers = pkgs.vimPlugins.nvim-treesitter.passthru.builtGrammars;
-    in
-    {
-      terraform = {
-        extensions = [
-          "hcl"
-          "tf"
-        ];
-        library = parsers.terraform;
-      };
-      xml = {
-        extensions = [
-          "xml"
-        ];
-        library = parsers.xml;
-      };
-    };
 }
