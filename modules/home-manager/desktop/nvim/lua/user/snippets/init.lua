0cecfacd7df31c9d04229d12e3baf8243057e1db
Nathan Regner <nathanregner@gmail.com>
2026-01-31 22:21:18 -0700

    feat(nvim): add nix snippets

--- modules/home-manager/desktop/nvim/lua/user/snippets/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/snippets/init.lua
@@ -1,6 +1,7 @@
 require("user.snippets.all")
 require("user.snippets.ecma")
 require("user.snippets.java")
+require("user.snippets.nix")
 require("user.snippets.rust")
 
 for _, module in ipairs(vim.g.nix.luasnip.extraModules or {}) do
