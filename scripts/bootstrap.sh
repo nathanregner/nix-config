5a44c8ba6ba2f1daba85d758ebf1beb922fd73cb
Nathan Regner <nathanregner@gmail.com>
2026-02-04 23:09:47 -0700

    feat(nix): enable pipe-operators

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,1 +1,1 @@
-nix --extra-experimental-features nix-command --extra-experimental-features flakes develop .#bootstrap
+nix --extra-experimental-features nix-command --extra-experimental-features flakes --extra-experimental-features pipe-operators develop .#bootstrap
