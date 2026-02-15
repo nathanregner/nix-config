2b56fb06a93ce3c188cb7984f0cfd535845ab0fd
Nathan Regner <nathanregner@gmail.com>
2026-02-15 13:22:45 -0700

    build: update flake.lock

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
 
     # Misc
     catppuccin-nix = {
-      url = "github:catppuccin/nix";
+      url = "github:catppuccin/nix/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     deploy-rs = {
