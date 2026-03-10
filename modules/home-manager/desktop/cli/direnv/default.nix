a33b15df99aa73ddf84229f4b301069ba1e50749
Nathan Regner <nathanregner@gmail.com>
2026-03-09 19:03:32 -0600

    feat(home): add amux

--- modules/home-manager/desktop/cli/direnv/default.nix
+++ modules/home-manager/desktop/cli/direnv/default.nix
@@ -9,4 +9,8 @@
   };
 
   xdg.configFile."direnv/lib/_layout.sh".source = config.lib.file.mkFlakeSymlink ./_layout.sh;
+
+  programs.claude-code.merged-hooks.PreToolUse = [
+    { command = ''DIRENV_LOG_FORMAT="-" direnv exec . $CMD''; }
+  ];
 }
