734e3c8759d2e8ffe645e13801223c9dee7526f3
Nathan Regner <nathanregner@gmail.com>
2026-03-17 17:29:39 -0600

    wip: feat(nvim): filter unused grammars

--- modules/home-manager/desktop/nvim/modules/java/java.lua
+++ modules/home-manager/desktop/nvim/modules/java/java.lua
@@ -1,16 +1,10 @@
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-local workspace_dir = vim.fn.environ()["HOME"] .. "/.cache/jdtls/workspace/" .. project_name
-
 ---@type vim.lsp.ClientConfig
 local config = {
   cmd = {
     "jdtls",
     "--jvm-arg=-javaagent:" .. vim.g.nix.jdtls.lombok,
     "-Dlog.level=WARNING",
-    "-data",
-    workspace_dir,
   },
-
   root_dir = vim.fs.root(0, { ".git", "mvnw", "gradlew" }),
   settings = vim.g.nix.jdtls.settings,
   ---@diagnostic disable-next-line: missing-fields
