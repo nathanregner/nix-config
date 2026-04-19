4dd64f0ee1979aab9261bf72f04ae435921939f0
Nathan Regner <nathanregner@gmail.com>
2026-04-19 10:37:21 -0600

    feat(nvim): replace ufo with native folds

--- modules/home-manager/desktop/nvim/lua/user/init.lua
+++ modules/home-manager/desktop/nvim/lua/user/init.lua
@@ -153,73 +153,6 @@ require("lazy").setup({
   },
 
   {
-    "kevinhwang91/nvim-ufo",
-    dependencies = { "kevinhwang91/promise-async" },
-    lazy = false,
-    config = function()
-      local handler = function(virtText, lnum, endLnum, width, truncate)
-        local newVirtText = {}
-        local suffix = (" 󰁂 %d "):format(endLnum - lnum)
-        local sufWidth = vim.fn.strdisplaywidth(suffix)
-        local targetWidth = width - sufWidth
-        local curWidth = 0
-        for _, chunk in ipairs(virtText) do
-          local chunkText = chunk[1]
-          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-          if targetWidth > curWidth + chunkWidth then
-            table.insert(newVirtText, chunk)
-          else
-            chunkText = truncate(chunkText, targetWidth - curWidth)
-            local hlGroup = chunk[2]
-            table.insert(newVirtText, { chunkText, hlGroup })
-            chunkWidth = vim.fn.strdisplaywidth(chunkText)
-            -- str width returned from truncate() may less than 2nd argument, need padding
-            if curWidth + chunkWidth < targetWidth then
-              suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
-            end
-            break
-          end
-          curWidth = curWidth + chunkWidth
-        end
-        table.insert(newVirtText, { suffix, "MoreMsg" })
-        return newVirtText
-      end
-
-      ---@diagnostic disable-next-line: missing-fields
-      require("ufo").setup({
-        open_fold_hl_timeout = 150,
-        close_fold_kinds_for_ft = {
-          default = { "imports", "comment" },
-        },
-        preview = {
-          win_config = {
-            winblend = 0,
-          },
-          mappings = {
-            close = "q",
-            switch = "K",
-          },
-        },
-        fold_virt_text_handler = handler,
-        ---@diagnostic disable-next-line: unused-local
-        provider_selector = function(bufnr, filetype, buftype)
-          -- vim.print({ _bufnr, _filetype, buftype })
-          if buftype == "" or buftype == nil then return { "treesitter", "indent" } end
-          return ""
-        end,
-      })
-      vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-      vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-      vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
-      vim.keymap.set("n", "zm", require("ufo").closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
-      vim.keymap.set("n", "K", function()
-        local winid = require("ufo").peekFoldedLinesUnderCursor()
-        if not winid then vim.lsp.buf.hover() end
-      end)
-    end,
-  },
-
-  {
     "AndrewRadev/bufferize.vim",
     event = "CmdlineEnter",
     config = function() vim.api.nvim_create_user_command("Msgs", "Bufferize messages", { desc = "Bufferize messages" }) end,
@@ -1020,9 +953,21 @@ vim.o.completeopt = "menuone,noselect"
 vim.o.termguicolors = true
 
 vim.o.foldcolumn = "0"
-vim.o.foldlevel = 99 -- ufo needs a large value
+vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+-- Custom fold text: first line + " 󰁂 N " count (matches former ufo format)
+_G._foldtext = function()
+  local lnum = vim.v.foldstart
+  local endLnum = vim.v.foldend
+  local line = vim.api.nvim_buf_get_lines(0, lnum - 1, lnum, false)[1] or ""
+  local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+  return { { line, "Normal" }, { suffix, "MoreMsg" } }
+end
+vim.opt.foldtext = "v:lua._foldtext()"
 
 -- [[ Basic Keymaps ]]
 
