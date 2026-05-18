d6f233dd36701788ad222a892cbf9155e105f875
Nathan Regner <nathanregner@gmail.com>
2026-05-18 06:50:43 -0600

    feat(home): update btop config

--- modules/home-manager/desktop/cli/btop.nix
+++ modules/home-manager/desktop/cli/btop.nix
@@ -2,7 +2,12 @@
   programs.btop = {
     enable = true;
     settings = {
-      "vim_keys" = true;
+      mem_graphs = false;
+      proc_per_core = true;
+      show_swap = true;
+      swap_disk = false;
+      show_disks = false;
+      vim_keys = true;
     };
   };
   catppuccin.btop.enable = true;
