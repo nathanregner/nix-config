289f25353e246d963acbad692cde244cc11ef63d
Nathan Regner <nathanregner@gmail.com>
2026-05-23 12:17:17 -0600

    build(flake): update packages

--- pkgs/sf-mono-nerd-font/package.nix
+++ pkgs/sf-mono-nerd-font/package.nix
@@ -5,13 +5,13 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "sf-mono-nerd-font";
-  version = "18.0d1e1.0";
+  version = "15.0d5e1";
   src = fetchFromGitHub {
     owner = "epk";
     repo = "SF-Mono-Nerd-Font";
     rev = "v${version}";
     fetchSubmodules = false;
-    sha256 = "sha256-f5A/vTKCUxdMhCqv0/ikF46tRrx5yZfIkvfExb3/XEQ=";
+    sha256 = "sha256-IkTbd5qpWue9utkCVHTvPSHnrVLBU3OQ9BqorNU7yQk=";
   };
 
   dontBuild = true;
