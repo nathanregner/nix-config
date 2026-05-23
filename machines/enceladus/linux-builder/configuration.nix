a738be380d11a63cd042d2fc3ea8fa2426b3ca51
Nathan Regner <nathanregner@gmail.com>
2026-05-23 12:41:10 -0600

    fix(darwin): sync builder vm time on wake

--- machines/enceladus/linux-builder/configuration.nix
+++ machines/enceladus/linux-builder/configuration.nix
@@ -10,11 +10,26 @@
 
   networking.hostName = "enceladus-linux-vm";
 
-  users.users = {
-    builder.openssh.authorizedKeys.keys = lib.attrValues self.globals.ssh.allKeys;
-    builder.extraGroups = [ "wheel" ];
-    builder.password = "builder";
+  users.users.timesync = {
+    isSystemUser = true;
+    group = "timesync";
+    shell = "/run/current-system/sw/bin/bash";
+    openssh.authorizedKeys.keys = [ self.globals.ssh.hostKeys.enceladus ];
   };
+
+  users.groups.timesync = { };
+
+  security.sudo.extraRules = [
+    {
+      users = [ "timesync" ];
+      commands = [
+        {
+          command = "/run/current-system/sw/bin/date";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
 
   # don't rebuild vm image on every commit
   system.nixos.tags = lib.mkForce [ ];
--- machines/enceladus/linux-builder/hardware-configuration.nix
+++ machines/enceladus/linux-builder/hardware-configuration.nix
@@ -5,11 +5,5 @@
     cores = 8; # TODO: Figure out why this can't be > 8
     diskSize = lib.mkForce (64 * 1024);
     memorySize = lib.mkForce 8192;
-
-    # https://www.reddit.com/r/qemu_kvm/comments/lboxit/time_sync_macos_host_linux_guest
-    qemu.options = [
-      "-rtc"
-      "base=localtime,clock=host"
-    ];
   };
 }
