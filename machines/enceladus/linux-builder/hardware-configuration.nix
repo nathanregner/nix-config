cd16d7455279b6799ac1b3777cd145399220cdd6
Nathan Regner <nathanregner@gmail.com>
2026-05-23 12:41:10 -0600

    fix(darwin): enable builder vm time sync

--- machines/enceladus/linux-builder/hardware-configuration.nix
+++ machines/enceladus/linux-builder/hardware-configuration.nix
@@ -1,15 +1,13 @@
-{ lib, ... }:
+{ modulesPath, lib, ... }:
 {
+  imports = [ "${modulesPath}/virtualisation/qemu-guest-agent.nix" ];
+
   nixpkgs.hostPlatform = "aarch64-linux";
   virtualisation = {
     cores = 8; # TODO: Figure out why this can't be > 8
     diskSize = lib.mkForce (64 * 1024);
     memorySize = lib.mkForce 8192;
-
-    # https://www.reddit.com/r/qemu_kvm/comments/lboxit/time_sync_macos_host_linux_guest
-    qemu.options = [
-      "-rtc"
-      "base=localtime,clock=host"
-    ];
   };
+
+  services.qemuGuest.enable = true;
 }
