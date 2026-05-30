b3b737e5e9269c268e7ed688d74a4dfbc69feb85
Nathan Regner <nathanregner@gmail.com>
2026-05-30 07:43:21 -0600

    build(flake): update inputs

--- machines/callisto/configuration.nix
+++ machines/callisto/configuration.nix
@@ -70,6 +70,7 @@
 
   virtualisation.docker = {
     enable = true;
+    package = pkgs.unstable.docker;
     # enableOnBoot = false; # lazy start with docker.socket
     # extraOptions = "--insecure-registry sagittarius:5000";
     daemon.settings = {
