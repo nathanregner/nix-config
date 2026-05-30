9998ec8852f5d4f0b581b758832b174c72b54198
Nathan Regner <nathanregner@gmail.com>
2026-05-30 07:41:53 -0600

    feat(nvim): remove unused plugins

--- modules/home-manager/desktop/nvim/lua/user/plugins/bufferize.lua
+++
@@ -1,7 +0,0 @@
----@module "lazy"
----@type LazySpec
-return {
-  "AndrewRadev/bufferize.vim",
-  event = "CmdlineEnter",
-  config = function() vim.api.nvim_create_user_command("Msgs", "Bufferize messages", { desc = "Bufferize messages" }) end,
-}
--- modules/home-manager/desktop/nvim/lua/user/plugins/lsp-file-operations.lua
+++
@@ -1,9 +0,0 @@
----@module "lazy"
----@type LazySpec
-return {
-  "antosha417/nvim-lsp-file-operations",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  config = function() require("lsp-file-operations").setup() end,
-}
