ea8c6fab79a3d6fd5a1e1b73f3379bf7b0e9d36e
Nathan Regner <nathan.regner@clickbank.com>
2026-07-14 12:57:47 -0600

    Revert "fixup! feat(nvim): enable clojure-lsp jvm stats"
    This reverts commit ea989b3061265bd816b6e143e8e902de0773dbc7.

--- overlays/shared/clojure-lsp.nix
+++ overlays/shared/clojure-lsp.nix
@@ -3,11 +3,4 @@ pkg.overrideAttrs (oldAttrs: {
   nativeImageArgs = oldAttrs.nativeImageArgs ++ [
     "--enable-monitoring=jvmstat,heapdump,jfr"
   ];
-
-  nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ prev.makeWrapper ];
-
-  postInstall = (oldAttrs.postInstall or "") + ''
-    wrapProgram $out/bin/${oldAttrs.meta.mainProgram} \
-      --add-flags "-XX:+ExplicitGCInvokesConcurrent"
-  '';
 })
