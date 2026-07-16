6eb435bd15e8762e5ee56af5a08ec0f9b6c300da
Nathan Regner <nathan.regner@clickbank.com>
2026-07-16 14:26:30 -0600

    Revert "fix(nvim): use default iskeyword option"
    This reverts commit 4b79a03a1fee66553ffcdaff8828cf13374c8915.

--- modules/home-manager/desktop/nvim/after/ftplugin/clojure.lua
+++
@@ -1,5 +0,0 @@
--- the builtin clojure ftplugin does `setlocal iskeyword+=?,-,*,!,+,/,=,<,>,.,:,$,%,&,|`,
--- vim.opt_local.iskeyword:remove({ "?", "-", "*", "!", "+", "/", "=", "<", ">", ".", ":", "$", "%", "&", "|" })
--- so reset iskeyword to the normal default word scope.
--- keep ":" so keywords like :foo/bar are treated as a single word
-vim.opt_local.iskeyword = { "@", "48-57", "_", "192-255", ":" }
