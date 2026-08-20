0dfd7a82346ba4cd3624e14a1f4ff2f5487c76a0
Nathan Regner <nathanregner@gmail.com>
2026-08-20 07:50:01 -0600

    perf(zsh): disable oh-my-zsh

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -9,10 +9,6 @@
 
   users.mutableUsers = true;
 
-  programs.zsh = {
-    enable = true;
-    enableBashCompletion = true;
-  };
   environment.pathsToLink = [ "/share/zsh" ]; # as required by home-manager
   users.users.nregner.shell = pkgs.zsh; # login shell
 }
